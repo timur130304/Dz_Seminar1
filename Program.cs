52969b227c9ff04ada95360f015420196b78a4d1
Timur Aslanov <timurckaaslanov@yandex.ru>
2022-10-14 16:40:45 +0300

    Add HomeWork

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿//ЗАДАЧА 2
+
+/*Console.Write("Enter the number 1: ");
+int num1 = Convert.ToInt32( Console.ReadLine());
+Console.Write("Enter the number 2: ");
+int num2 = Convert.ToInt32( Console.ReadLine());
+if (num1 > num2){
+    Console.WriteLine($"The maximum number is {num1}");
+    Console.WriteLine($"The minimum number is {num2}");
+}
+else if(num1 < num2){
+    Console.WriteLine($"The maximum number is {num2}");
+    Console.WriteLine($"The minimum number is {num1}");
+}
+else{
+    Console.WriteLine($"{num1} = {num2}");
+}*/
+
+//ЗАДАЧА 4
+
+/*Console.Write("Enter the number 1: ");
+int num1 = Convert.ToInt32( Console.ReadLine());
+Console.Write("Enter the number 2: ");
+int num2 = Convert.ToInt32( Console.ReadLine());
+Console.Write("Enter the number 3: ");
+int num3 = Convert.ToInt32( Console.ReadLine());
+int max = num1;
+if (num2 > max){
+    max = num2;
+} 
+if (num3 > max){
+    max = num3;
+}
+Console.WriteLine($"The maximum number is {max}");*/
+
+//ЗАДАЧА 6
+
+/*Console.Write("Enter the number 1: ");
+int num = Convert.ToInt32( Console.ReadLine());
+if (num % 2 == 0){
+    Console.WriteLine($"The number {num} is even.");
+}
+else{
+    Console.WriteLine($"The number {num} is not even.");
+}*/
+
+//ЗАДАЧА 8
+
+/*Console.Write("Enter the number 1: ");
+int num = Convert.ToInt32( Console.ReadLine());
+int current = num;
+for(int i = 1; i <= num; i++){
+    if (current % 2 == 0){
+        Console.WriteLine(current);
+    }
+    current--;
+
+}*/
+
